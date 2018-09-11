0c653cc174c6a775dd69f59f5eb19947b1a1933b
yogi1048 <43023664+yogi1048@users.noreply.github.com>
2018-09-11 12:42:47 +0530

    Add files via upload

---
+++ godson.py
@@ -0,0 +1,9 @@
+a=int(input("enter value of a:"))
+b=int(input("enter value of b:"))
+
+if(a>b):
+	print(a,"is greater than",b)
+elif(a==b):
+	print(a,"is equal to",b)
+elif(b>a):
+	print(b,"is greater than",a)
