fb26e63ba87f414dc16c3ff156adc42fc1edc71b
yogi1048 <43023664+yogi1048@users.noreply.github.com>
2018-09-20 10:14:27 +0530

    Add files via upload

---
+++ godson6.py
@@ -0,0 +1,14 @@
+a=int(input("enter value of a"))
+if (a>=90):
+	print("A+")
+elif(a>=80):
+	print("A")
+elif(a>=70):
+	print("B+")
+elif(a>=60):
+	print("B")
+elif(a>=40):
+	print("C")
+else:
+	print("fail")
+	
