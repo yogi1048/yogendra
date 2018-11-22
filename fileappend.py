4f3c9000166efb547f16d0d501ca0ac1d0b7af1d
yogi1048 <43023664+yogi1048@users.noreply.github.com>
2018-11-22 09:36:50 +0530

    Add files via upload

---
+++ fileappend.py
@@ -0,0 +1,4 @@
+f=open("dl2.txt","a")
+f.write("programming with python")
+f=open("dl2.txt","r")
+print(f.read())
---
+++ filewrite.py
@@ -0,0 +1,4 @@
+f=open("dl2.txt","w")
+f.write("bye")
+f=open("dl2.txt","r")
+print(f.read())
